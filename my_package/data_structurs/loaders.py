0d59903b03735a85f0a4a6054c63121dd4206468
margelse <obydennikovall04@gmail.com>
2026-03-17 11:06:14 +0300

    debug LoaderCSVFilesObject

--- my_package/data_structurs/loaders.py
+++ my_package/data_structurs/loaders.py
@@ -4,37 +4,44 @@ import os
 class LoaderCSVFilesObject:
     def __init__(
             self,
-            dir,
+            directory,
             separation:str,
     ):
-        self.dir = dir
+        self.directory = directory
         self.separation = separation
 
-        self._get_file_paths()
-        self.delete_files = self._check_format_files()
+        self._init_sorted_files()
+
+    def _init_sorted_files(self):
+        all_file_paths = self._get_file_paths()
+        self.valid_file_paths, self.delete_files = self._search_not_csv_files(all_file_paths)
 
     def _get_file_paths(self):
-        self.files_from_dir = sorted(os.listdir(dir))
-        self.file_paths = [os.path.join(self.dir, path) for path in self.files_from_dir]
+        files_from_dir = sorted(os.listdir(self.directory))
+        file_paths = [os.path.join(self.directory, path) for path in files_from_dir]
 
-    def _check_format_files(self):
+        return file_paths
+
+    def _search_not_csv_files(self, all_paths):
         VALID_FORMAT = '.csv'
         delete_files = []
+        valid_file_paths = []
 
-        for idx, path in enumerate(self.file_paths):
+        for path in all_paths:
             _, format_file = os.path.splitext(path)
-            if format_file == VALID_FORMAT:
-                continue
-            else:
-                file = self.file_paths.pop(idx)
-                delete_files.append(file)
 
-        return delete_files
+            if format_file == VALID_FORMAT:
+                valid_file_paths.append(path)
+
+            else:
+                delete_files.append(path)
+
+        return valid_file_paths, delete_files
     
     def description(self):
         TEMPLATE = {
-            'directory': self.dir,
-            'file paths': self.file_paths,
+            'directory': self.directory,
+            'sorted file paths': self.valid_file_paths,
             'delete files': self.delete_files
         }
 
